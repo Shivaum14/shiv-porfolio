5096e2572a448603a041261250709decd91f52f9
Shiv <Shivaumm@gmail.com>
2022-08-16 23:01:08 +0100

    Upgraded NavBar

---
+++ src/components/Home.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import HeroImage from "../assets/profile.jpg";
+import { RiArrowRightLine } from "react-icons/ri";
+
+function Home() {
+	return (
+		<div
+			name="home"
+			className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+		>
+			<div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+				<div className="flex flex-col justify-center h-full">
+					<h2 className="text-4xl sm:text-7xl font-bold text-white">
+						I'm a Full Stack Developer{" "}
+					</h2>
+					<p className="text-gray-500 py-4 max-w-md">
+						A recent software engineering graduate from King’s College London with an
+						extensive background in software design, development, and testing. I am a
+						motivated individual seeking to use my experience in a fast-paced software
+						development role where I can continue to develop my skills and technical
+						knowledge while contributing to complex large scale projects.
+					</p>
+					<div>
+						<button className="text-white w-fit px-6 py-3 my-6 flex  items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer group">
+							Portfolio
+							<span className="group-hover:rotate-90 duration-300">
+								<RiArrowRightLine size={23} className="ml-1" />
+							</span>
+						</button>
+					</div>
+				</div>
+				<div>
+					<img
+						src={HeroImage}
+						alt="my profile"
+						className="rounded-2xl mx-auto w-2/3 md:w-[70%]"
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Home;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
+/**
+ *
+ * @returns jsx navigation bar compnent
+ */
 function NavBar() {
+	const [nav, setNav] = useState(false); //State for expanable nav bar
+
+	const links = [
+		{ id: 1, link: "home" },
+		{ id: 2, link: "about" },
+		{ id: 3, link: "portfolio" },
+		{ id: 4, link: "experience" },
+		{ id: 5, link: "contact" },
+	]; // Nav nar links
+
 	return (
-		<div>
-			<h1>hello</h1>
+		<div className="flex justify-between items-center w-full h-20 text-white bg-black px-4 fixed">
+			<div>
+				<h1 className="text-5xl font-signature ml-2">Shivaum</h1>
+			</div>
+			<ul className="hidden md:flex">
+				{links.map(({ id, link }) => {
+					return (
+						<li
+							key={id}
+							className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+						>
+							{link}
+						</li>
+					);
+				})}
+			</ul>
+			<div
+				onClick={() => setNav(!nav)}
+				className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+			>
+				{nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+			</div>
+
+			{/* Handles logic for expandable nav bar 
+			{nav && (
+				<ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+					{links.map(({ id, link }) => {
+						return (
+							<li key={id} className="px-4 cursor-pointer capitalize py-6 text-4xl">
+								{link}
+							</li>
+						);
+					})}
+				</ul>
+			)}*/}
+
+			<ul
+				className={
+					(nav ? "translate-x-0" : "translate-x-full") +
+					" " +
+					"flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500 duration-300"
+				}
+			>
+				{links.map(({ id, link }) => {
+					return (
+						<li
+							key={id}
+							className="px-4 cursor-pointer capitalize py-6 text-4xl hover:scale-125 duration-200"
+						>
+							{link}
+						</li>
+					);
+				})}
+			</ul>
 		</div>
 	);
 }
