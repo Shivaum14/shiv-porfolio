129c2fba33bba703b04e6b7b0dbdd985043599fc
Shiv <Shivaumm@gmail.com>
2022-08-16 19:03:22 +0100

    Initialised tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import "./App.css";
-
 function App() {
 	return (
-		<div>
-			<h1>Hello World</h1>
+		<div className="">
+			<h1 className="text-4xl font-bold font-signature">Hello World</h1>
 		</div>
 	);
 }
