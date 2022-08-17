76b743a26fbb4b4ce95e150aab3d6e135313911d
Shiv <Shivaumm@gmail.com>
2022-08-17 23:09:42 +0100

    mobile bug fixes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -83,8 +83,11 @@ function Experience() {
 	};
 
 	return (
-		<div name="experience" className="w-full bg-gradient-to-b from-gray-800 to-black h-screen">
-			<div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full text-white">
+		<div
+			name="experience"
+			className="w-full bg-gradient-to-b from-gray-800 to-black h-screen pb-12 md:pb-0"
+		>
+			<div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full text-white pb-12 md:pb-0">
 				<div className="pb-8">
 					<p className="text-4xl font-bold inline border-b-4 border-gray-500 p-2">
 						Experience
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,11 +7,11 @@ function Home() {
 	return (
 		<div
 			name="home"
-			className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+			className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 pt-12 lg:pt-0"
 		>
 			<div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-				<div className="flex flex-col justify-center h-full">
-					<h2 className="text-4xl sm:text-7xl font-bold text-white">
+				<div className="flex flex-col justify-center h-full pt-12 lg:pt-0">
+					<h2 className="text-4xl md:text-7xl font-bold text-white pt-12 lg:pt-0">
 						I'm a Full Stack Developer{" "}
 					</h2>
 					<p className="text-gray-500 py-4 max-w-md">
